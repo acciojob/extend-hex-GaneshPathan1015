95943ad41d8092c568456c208b06c7115b053a2c
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-09 20:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const extendHex = (shortHex) => {
+const extendHex = (s) => {
   // write your code here
+	let iscaptial = false;
+	let ans = `#`;
+	let i = s.length-3;
+
+	while (i<s.length){
+		let asci = s[i].charCodeAt(0);
+		if(asci>=65 && asci<=90){
+			iscaptial=true;
+		}
+		ans=ans+s[i]+s[i];
+		i++;
+	}
+	return iscaptial?ans.toUpperCase(): ans;
 };
 
 // Do not change the code below.
